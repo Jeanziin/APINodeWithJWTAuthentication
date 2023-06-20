c6c28551d606c6f2cfe315b94f352be5f629be2b
Jeanziin <jjeanfreitass@gmail.com>
2023-06-20 09:22:50 -0300

    database connection

--- API/src/server.js
+++ API/src/server.js
@@ -1,6 +1,12 @@
+const { default: mongoose } = require('mongoose');
+const connection = require('./models/connection');
 const app = require('./app');
 require('dotenv').config();
 
 const PORT = 3333;
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+connection.then(() => {
+    
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT} and connected database`));
+
+}).catch((err) => console.log(err));
